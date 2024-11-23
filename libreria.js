95607149bf246b3c11e31a0faf97ce2b9f4a0ea7
leo <heiberescalona06@gmail.com>
2024-11-23 10:43:19 -0400

    ejemplo de el arreglo

--- libreria.js
+++ libreria.js
@@ -1,1 +1,25 @@
 //hola mundo
+let librosColeccion = [
+    {
+        nombre: "Cien Años de Soledad",
+        paginas: 417,
+        autor: "Gabriel García Márquez",
+        generos: ["Realismo mágico", "Literatura latinoamericana"],
+        disponible: true
+    },
+    {
+        nombre: "1984",
+        paginas: 328,
+        autor: "George Orwell",
+        generos: ["Distopía", "Ciencia ficción"],
+        disponible: false
+    }
+];
+
+   function informacionLibro (libro){
+        let generos = libros.generos.join(", ");
+        let 
+
+   }
+
+console.log(librosColeccion);
